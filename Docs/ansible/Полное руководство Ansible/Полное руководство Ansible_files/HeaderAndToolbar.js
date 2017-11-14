1eb2df54c58dcf3b3c21dc7662a485f71fd02dff
ara7788 <test3007@ya.ru>
2017-11-14 22:43:58 +0200

    pdf/Advanced-Bash-Scripting-Guide.pdf Redis Ansible

---
+++ Docs/ansible/Полное руководство Ansible/Полное руководство Ansible_files/HeaderAndToolbar.js
@@ -0,0 +1,34 @@
+HeaderAndToolbar=  ''+
+'<div id="header">'+ runningString +
+'	<a onclick="_gaq.push([\'_trackEvent\', \'Header\', \'logo\', \'click\', 1]);" align="middle" target="_blank" href="http://www.mdl.ru">'+
+'	<img src="/i/mdl-reg.jpg" style="position: relative; border-style: none; top: 18%;" alt="Перевод Mdl&reg;" width="40" height="14" align="middle"/></a>'+
+'	<a onclick="_gaq.push([\'_trackEvent\', \'Header\', \'logo\', \'click\', 1]);" target="_blank" href="https://www.packtpub.com/virtualization-and-cloud/">'+
+'	<img src="/common/images/PacktPub-logo.jpg" alt="Документация Virtualization and Cloud" width="109" height="44"/></a>'+
+''+
+'	<h1>'+HeaderName+'</h1>'+
+'	<div id="navheader" align="right"><!----><table class="navLinks"><tr>'+
+'		<td><a id="showHideButton" onclick="showHideToc(); _gaq.push([\'_trackEvent\', \'Header\', \'show/hide\', \'click\', 1]);" class="pointLeft" title="Скрыть дерево оглавления">Боковое поле</a>'+
+'		</td><td><a accesskey="p" class="navLinkPrevious" onclick="_gaq.push([\'_trackEvent\', \'Header\', \'prevLink\', \'click\', 1]);" tabindex="5" href="'+PrevRef+'">Предыдущий</a> '+
+'		  |'+
+'       <a accesskey="u" class="navLinkUp" onclick="_gaq.push([\'_trackEvent\', \'Header\', \'upLink\', \'click\', 1]);" tabindex="5" href="'+UpRef+'">Вверх</a>'+
+'		  |'+
+'        <a accesskey="n" class="navLinkNext" onclick="_gaq.push([\'_trackEvent\', \'Header\', \'nextLink\', \'click\', 1]);" tabindex="5" href="'+NextRef+'">Следующий</a>'+
+'		</td></tr></table>'+
+'	</div>'+
+'</div>'+
+'<div id="toolbar" class="ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all">'+
+'	<div id="toolbar-left">'+
+'		<p class="breadcrumbs"><a href="'+ cloudcomputingURL +'">'+ cloudcomputingURLText +'</a>'+
+'		<a href="http://onreader.mdl.ru/KVMVirtualizationCookbook/content/Ch08.html">Книга рецептов виртуализации KVM</a>'+
+'		<a href="http://onreader.mdl.ru/MasteringPythonNetworking/content/Ch04.html">Полное руководство работы с сетями на Python</a>'+
+'		(<a onclick="_gaq.push([\'_trackEvent\', \'Header\', \'pdfDownload\', \'click\', 1]);" alt="Купите оригинал" class="pdficon" href="https://www.packtpub.com/networking-and-servers/mastering-ansible-second-edition">'+
+'		<img src="/common/images/pdf.png"/></a>) &nbsp; '+
+'		</p>'+
+'		<div id="gplusone">'+
+'			<a id="AskMdl1" href="javascript:tocall()" onmouseover="this.href=mail" target="_blank" title="Вопросы? Обсудите их на mdl.ru">'+
+'			<i class="icon-question"><!----></i></a>  '+
+'			<a id="logABugLink1" href="javascript:tocall()" onmouseover="this.href=mail" title="Нашли ошибку? Сообщите о ней на данной странице">'+
+'			<i class="icon-bug"><!----></i></a>'+
+'		</div>'+
+'	</div>'+
+'</div>';
